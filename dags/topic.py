d5ba40a6077e03d94cc16ec2d1f70b7c7b604312
kietlu17 <kietnguyen17042004@gmail.com>
2024-07-28 14:54:26 +0700

    comment code

--- dags/topic.py
+++ dags/topic.py
@@ -1,3 +1,4 @@
+# Import các thư viện cần thiết
 import finnhub
 import json
 from datetime import datetime
@@ -7,11 +8,13 @@ from airflow.operators.python import PythonOperator
 import logging
 from confluent_kafka import Producer
 
+# Cấu hình các tham số cho Kafka
 KAFKA_BOOTSTRAP_SERVERS = ['broker:29092']
 KAFKA_TOPIC = "topic_finnhub"
 PAUSE_INTERVAL = 10
 STREAMING_DURATION = 120
 
+# Hàm cấu hình Kafka
 def configure_kafka(servers=KAFKA_BOOTSTRAP_SERVERS):
     settings = {
         'bootstrap.servers': ','.join(servers),
@@ -19,16 +22,19 @@ def configure_kafka(servers=KAFKA_BOOTSTRAP_SERVERS):
     }
     return Producer(settings)
 
+# Hàm kiểm tra trạng thái gửi message
 def delivery_status(err, msg):
     if err is not None:
         print('Gửi thất bại:', err)
     else:
         print('Message được gửi tới', msg.topic(), '[Partition: {}]'.format(msg.partition()))
 
+# Hàm gửi message tới Kafka
 def publish_to_kafka(producer, topic, data):
     producer.produce(topic, value=json.dumps(data).encode('utf-8'), callback=delivery_status)
     producer.flush()
 
+# Hàm lấy dữ liệu từ API và gửi tới Kafka
 def stream_data():
     finnhub_client = finnhub.Client(api_key="cqchq4pr01qoodgbgkbgcqchq4pr01qoodgbgkc0")
 
@@ -37,7 +43,7 @@ def stream_data():
     message = {
         'symbol': 'AAPL',
         'data': quote_data,
-        'timestamp': datetime.utcnow().isoformat()
+        'timestamp': datetime.now().isoformat()
     }
 
     print(message)
@@ -46,14 +52,16 @@ def stream_data():
 
     publish_to_kafka(producer, KAFKA_TOPIC, message)
 
+# Cấu hình DAG
 default_args = {
     'owner': 'airflow',
     'start_date': datetime(2024, 1, 1, 10, 00)
 }
 
+# Tạo DAG
 with DAG('finnhub_kafka',
          default_args=default_args,
-         schedule_interval='@once',
+         schedule_interval='* * * * *',
          catchup=False) as dag:
 
     streaming_task = PythonOperator(
@@ -61,5 +69,6 @@ with DAG('finnhub_kafka',
         python_callable=stream_data
     )
 
+# Kết nối các task trong DAG
 streaming_task
 
